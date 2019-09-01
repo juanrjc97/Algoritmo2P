c7d3c0f3ac2b0108450f8cfcbb0fd59da5681edd
juanj97 <juanj121297e@gmail.com>
2019-08-31 19:51:56 -0500

    p

---
+++ ProyectoAlgPar2/src/proyectoalgpar2/ProyectoAlgPar2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoalgpar2;
+
+/**
+ *
+ * @author juanjimenez
+ */
+public class ProyectoAlgPar2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("100 en proyecto sino me quedo :(");
+    }
+    
+}
